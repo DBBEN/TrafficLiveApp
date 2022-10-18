f5880078d2de2d6f64c543b0685910aba713b217
Dave Benemerito <iamdavebenemerito@gmail.com>
2022-10-19 00:39:57 +0800

    initial commit

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:429273454730:web:d8fdc7109d42ef736af573";
+  static String apiKey = "AIzaSyCe7xdjT33K3zFwtNkf0nUNRFOIkAZjyew";
+  static String messagingSenderId = "429273454730";
+  static String projectId = "trafficliveapp-fe661";
+  final primaryColor = const Color(0xFF0D6B45);
+}
